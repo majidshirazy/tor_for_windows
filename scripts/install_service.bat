33d44649853e863371824b5332735906759bb49f
Majid Shiraazi <majidshirazy@gmail.com>
2022-12-03 00:19:51 +0330

    Update install_service.bat

--- scripts/install_service.bat
+++ scripts/install_service.bat
@@ -16,7 +16,7 @@ CD /d %~dp0\..
 IF EXIST "%PROGRAMDATA%\TOR" DEL /S /Q "%PROGRAMDATA%\TOR"
 MKDIR "%PROGRAMDATA%\TOR"
 XCOPY /EY "*" "%PROGRAMDATA%\TOR\"
-DEL /Q /S %programdata%\TOR\scripts
+RMDIR /Q /S %programdata%\TOR\scripts
 sc create Tor start=auto binPath="%PROGRAMDATA%\TOR\tor.exe -nt-service -f %PROGRAMDATA%\TOR\torrc"
 sc description Tor "This is the TOR Windows Service. It can be used by Bridges such as snowflake or obfs4 by editing TOR config file located in C:\ProgramData\Tor\torrc"
 powershell -Command "net start Tor"
